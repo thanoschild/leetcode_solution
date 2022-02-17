a307f71d360b36113516da41c4f8fbeec1ce4030
godspell <99740668+godspell@users.noreply.github.com>
2022-02-17 11:43:42 +0530

    Create 39. Combination Sum.cpp

---
+++ 39. Combination Sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(int i, vector<int> &v, vector<vector<int>> &ans, int target, vector<int> &arr)
+    {
+     if(arr.size() == i)
+     {
+         if(target == 0)
+         ans.push_back(v);
+         return;
+     }
+     if(target > arr[i])
+     {
+         v.push_back(arr[i]);
+         solve(i, v, ans, target-arr[i], arr);
+         v.pop_back();
+     }
+     solve(i+1, v, ans, target, arr);
+    }
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        solve(0, v, ans, target, arr);
+        return ans;
+    }
+};
