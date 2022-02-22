523be3d019de932cb5eed019928501e44876ad27
godspell <99740668+godspell@users.noreply.github.com>
2022-02-22 23:47:56 +0530

    Create 239. Sliding Window Maximum.cpp

---
+++ 239. Sliding Window Maximum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+       if(k>nums.size()) return {};
+       int n = nums.size();
+       vector<int> ans;
+       deque<int> pq;
+       for(int i=0; i<n; i++){
+          while(!pq.empty() && i - pq.front() >= k)
+          pq.pop_front();
+          while(!pq.empty() && nums[pq.back()] < nums[i])
+          pq.pop_back();
+          pq.push_back(i);
+          if(i >=k -1) ans.push_back(nums[pq.front()]);
+       }
+       return ans;
+    }
+};
