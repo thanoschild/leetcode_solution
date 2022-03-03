ad157f258869ab3fec17a894d864e23445047406
godspell <99740668+godspell@users.noreply.github.com>
2022-03-03 09:27:55 +0530

    Create 413. Arithmetic Slices.cpp

---
+++ 413. Arithmetic Slices.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+       int n = nums.size();
+       if(n<= 2) return 0;
+       int count = 0;
+       int ans = 0;
+       for(int i = 2; i<n; i++){
+           if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) count++;
+           else count = 0;
+           ans += count;
+       } 
+       return ans;
+    }
+};
